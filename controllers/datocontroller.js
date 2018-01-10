92227456155913c0415cca58586d4e9d241ce48d
andresm090 <andres_m090@hotmail.com>
2018-01-10 01:00:06 -0300

    panel fotovoltaico

--- controllers/datocontroller.js
+++ controllers/datocontroller.js
@@ -1,16 +1,18 @@
 var passport = require('passport');
 var Comuna = require('../model/Comuna');
 var Generador = require('../model/Generador');
-//gauges
+//gauges 
 var gaugeTemp = require('../src/gaugeTemp');
 var gaugeVel = require('../src/gaugeVel');
 var gaugeVA = require('../src/gaugeVA');
 var gaugeWR = require('../src/gaugeWR');
 var statebarPN = require('../src/statebarPN');
+var gaugePira = require('../src/gaugePira');
+var stateVbb = require('../src/stateVbb');
 
 
 
-exports.getfPanelControl = (req, res, next) => {
+/*exports.getfPanelControl = (req, res, next) => {
 
 	Generador.findById(req.params.id, (err, generador) => {
 		if (err) {
@@ -29,6 +31,23 @@ exports.getfPanelControl = (req, res, next) => {
 				return res.send('No es lo que parece.');
 			}
 			
+		}
+	});
+};*/
+
+exports.getfPanelControl = (req, res, next) => {
+
+	Generador.findById(req.params.id, (err, generador) => {
+		if (err) {
+			return res.send('Ha surgido un error.');
+		} else {
+			res.locals.user = req.user;
+			Comuna.populate(generador, {path: "comuna"}, (err, generador) => {
+				if (err){
+					return res.send('No es lo que parece.');
+				}
+				return res.render('aerogenerador',  {titulo: generador.comuna.nombre + " - " + generador.tipo+" "+generador.caracteristicas[0]['modelo-serie'], id: generador.id});
+			});	
 		}
 	});
 };
@@ -52,11 +71,11 @@ exports.getTrDatosTR = (req, res, next) => {
 		if (err) {
 			return res.send('Ha surgido un error.');
 		} else {
+			res.locals.user = req.user;
 			if (generador.isAerogenerador()){
-				res.locals.user = req.user;
 				return res.render('tr_aero2', {gaugeTemp: gaugeTemp, gaugeVel: gaugeVel, gaugeVA: gaugeVA, gaugeWR: gaugeWR, statebarPN: statebarPN, potenciaN: generador.caracteristicas[0]['potencia']});
 			} else {
-				return res.send('Panel de panel fotovoltaico'); //COmpletar
+				return res.render('tr_panelf', {gaugeTemp: gaugeTemp, gaugePira: gaugePira, gaugeVA: gaugeVA, statebarPN: statebarPN, potenciaN: generador.caracteristicas[0]['potencia'], stateVbb: stateVbb}); //Completar
 			}
 		}
 
